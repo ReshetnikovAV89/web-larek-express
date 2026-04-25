1db9ccc463bf50106628a51653bd8d80e0f25a86
ReshetnikovAV89 <andrey.reshetnikov.89@mail.ru>
2026-04-25 17:36:02 +0800

    handle unknown routes with not found error

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,6 +8,7 @@ import { errors } from 'celebrate';
 import productRouter from './routes/product';
 import orderRouter from './routes/order';
 import authRouter from './routes/auth';
+import NotFoundError from './errors/not-found-error';
 import uploadRouter from './routes/upload';
 import errorHandler from './middlewares/error-handler';
 import { errorLogger, requestLogger } from './middlewares/logger';
@@ -32,6 +33,10 @@ app.use('/upload', uploadRouter);
 
 app.get('/', (_req, res) => {
   res.json({ message: 'Server is running' });
+});
+
+app.use((_req, _res, next) => {
+  next(new NotFoundError('Маршрут не найден'));
 });
 
 app.use(errors());
